d61c6921fb546cf335f5bdebd270401af0136321
Scepcop <scepcop@gmail.com>
2017-08-09 21:07:26 +0300

    Add files via upload

---
+++ testing/testing/main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <fstream>  
+#include <clocale>  
+#include <cstdlib>  
+
+int main(int argc, char * argv[]) {
+	setlocale(LC_ALL, "Rus");
+
+	char file_name[24];
+	std::cout << "Âגוהטעו טלÿ פאיכא: "; 
+	std::cin >> file_name;
+
+	std::ifstream file;
+	do {
+
+		std::cout << "\nÔאיכ םו םאיהום.\n"
+			<< "Âגוהטעו טלÿ פאיכא: ";
+		std::cin >> file_name;
+		file.open(file_name);
+		if (file.good())
+			break;
+	} while (true);
+
+	char   strBuff[124][124];
+	int    n_strok = 0;
+
+	std::cout << "\n";
+
+	char *str = new char[1024];
+	int i = 0;
+	while (!file.eof()) {
+		file.getline(str, 1024, '\n');
+		i++;
+	}
+	file.close();
+	delete str;
+	std::cout << i << '\n';
+	system("pause");
+	return 0;
+}
